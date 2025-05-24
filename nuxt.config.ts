fef640d8c4a8e0ae33aabfb4a220123bdb2774cb
Alexandre Hallaine <alexandre@hallaine.com>
2025-05-24 15:06:21 -0400

    feat: nuxt hub

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', '@nuxthub/core'],
+  hub: { workers: true },
+  nitro: { experimental: { openAPI: true } },
+  $development: { hub: { remote: true } },
+  app: { head: { title: 'RNCP' } },
 })
